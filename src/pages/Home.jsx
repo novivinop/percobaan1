6ad7e27d20b22ee781a4f8c486fe7c4e37b0a4e4
unknown <Ikadhanny>
2023-06-01 20:31:39 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,25 @@ import Testimoni from "../components/Testimoni";
 import Langganan from "../components/Langganan";
 import Footer from "../components/Footer";
 import Snk from "../components/Snk";
+import React, { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-G83CGRLQP3";
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Deklarasi variabel dataLayer
+    const dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "G-G83CGRLQP3");
+  }, []);
+
   return (
     <>
       <Header />
